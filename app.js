f9f28d6fac6b881bf5d3e3403f2ddc4d3abefd30
MeXx <mexx@devsub.net>
2015-02-27 15:48:33 +0100

    Sending values of DMX channel 1 as OSC message

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var UDP_PORT_ARTNET = 6454;
-var UDP_PORT_OSC = 1234;
+var UDP_PORT_OSC = 3333;
 var HTTP_PORT = 3000;
 
 
 // Setup UPD Port for Artnet
 var artnetsrv = require('./artnet_server');
+// Setup OSC
+var osc = require('node-osc');
+var oscClient = new osc.Client('0.0.0.0', UDP_PORT_OSC);
 
-var srv = artnetsrv.listen(6454, function(msg, peer) {
+var srv = artnetsrv.listen(UDP_PORT_ARTNET, function(msg, peer) {
 	console.log("-----------------");
 	console.log("Sequence: " + msg.sequence);
 	console.log("Physical: " + msg.physical);
@@ -14,4 +17,6 @@ var srv = artnetsrv.listen(6454, function(msg, peer) {
 	console.log("Length: " + msg.length);
 	console.log("Data: " + msg.data);
 	console.log("-----------------");
+
+	oscClient.send('/channels/1', msg.data[0]);
 });
