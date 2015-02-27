ade7d7f434cafbf5ed8a95d944a3c20771bfb701
MeXx <mexx@devsub.net>
2015-02-27 15:14:13 +0100

    Fixed artnet-server

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var HTTP_PORT = 3000;
 
 
 // Setup UPD Port for Artnet
-var artnetsrv = require('./node_modules/artnet-node/lib/artnet_server');
+var artnetsrv = require('./artnet_server');
 
 var srv = artnetsrv.listen(6454, function(msg, peer) {
 	console.log("-----------------");
