4536061d228a1d878707e3f955e78029fc624f16
MeXx <mexx@devsub.net>
2015-02-27 14:04:29 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# DMX2OSC
+
+Web-Server that receives ArtNet DMX and translates it to OSC Messages
+
+__WIP__
---
+++ app.js
@@ -0,0 +1,17 @@
+var UDP_PORT_ARTNET = 6454;
+var UDP_PORT_OSC = 1234;
+var HTTP_PORT = 3000;
+
+
+// Setup UPD Port for Artnet
+var artnetsrv = require('./node_modules/artnet-node/lib/artnet_server');
+
+var srv = artnetsrv.listen(6454, function(msg, peer) {
+	console.log("-----------------");
+	console.log("Sequence: " + msg.sequence);
+	console.log("Physical: " + msg.physical);
+	console.log("Universe: " + msg.universe);
+	console.log("Length: " + msg.length);
+	console.log("Data: " + msg.data);
+	console.log("-----------------");
+});
